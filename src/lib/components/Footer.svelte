bf6f9bea08fb3328013a646bab32c5d3df4ba32f
bswck <bartoszpiotrslawecki@gmail.com>
2024-12-15 02:15:50 +0100

    Reformat file

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,17 +1,17 @@
 <div class="mt-[5em] text-center">
-	<div class="flex justify-center p-[1em]">
-		<!-- todo: socials -->
-		<div></div>
-		<div></div>
-		<div></div>
-	</div>
-	<p>Developed by<!-- --> <a href="https://github.com/bswck">@bswck</a></p>
-	<p>
-		Based on <a href="https://http.cat">http.cat</a> developed by<!-- -->
-		<a href="https://twitter.com/rogeriopvl">@rogeriopvl</a>
-	</p>
-	<p>
-		Original Images by<!-- --> Tomomi Imura (<a href="https://twitter.com/girlie_mac">@girlie_mac</a
-		>)
-	</p>
+  <div class="flex justify-center p-[1em]">
+    <!-- todo: socials -->
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+  <p>Developed by<!-- --> <a href="https://github.com/bswck">@bswck</a></p>
+  <p>
+    Based on <a href="https://http.cat">http.cat</a> developed by<!-- -->
+    <a href="https://twitter.com/rogeriopvl">@rogeriopvl</a>
+  </p>
+  <p>
+    Original Images by<!-- --> Tomomi Imura (<a href="https://twitter.com/girlie_mac">@girlie_mac</a
+    >)
+  </p>
 </div>
