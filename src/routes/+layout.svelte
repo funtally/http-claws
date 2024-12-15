5efa5ffa90436545e444d8783a8833b0fc0f51be
bswck <bartoszpiotrslawecki@gmail.com>
2024-12-15 02:16:21 +0100

    Add support for dynamic, page-specific titles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,21 @@
 <script>
-	import { Nav, Footer } from '$lib/components';
-	import '../app.css';
-	let { children } = $props();
+  import { page } from '$app/stores';
+  import { Nav, Footer } from '$lib/components';
+
+  import '../app.css';
+  let { children } = $props();
+
+  const pageName = $derived($page.data.name);
+  const siteName = 'HTTP Claws Game';
+  const titleSuffix = $derived(pageName ? ' • ' + pageName : '');
 </script>
 
-<div class="p-4 sm:px-16 sm:py-4 lg:px-32 lg:py-4">
-	<Nav />
-	{@render children()}
-	<Footer />
+<svelte:head>
+  <title>{siteName}{titleSuffix}</title>
+</svelte:head>
+
+<div class="m-auto max-w-screen-2xl p-4 sm:px-16 sm:py-4 lg:px-32 lg:py-4">
+  <Nav />
+  {@render children()}
+  <Footer />
 </div>
