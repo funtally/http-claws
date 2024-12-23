5939b88d708570ce1da65f105129476f5d147eaa
bswck <bartoszpiotrslawecki@gmail.com>
2024-12-23 03:00:29 +0100

    Implement game mode logic

--- src/lib/components/Game.svelte
+++ src/lib/components/Game.svelte
@@ -1,7 +1,76 @@
 <script>
-  import { randomImage } from '$lib/game';
+  import { randomHttpCode, imageForHttpCode, phraseForHttpCode } from '$lib/game';
+  import { scale } from 'svelte/transition';
+
+  let httpCode = $state(0);
+  let imageSrc = $derived(httpCode ? imageForHttpCode(httpCode) : 'cats/frame.png');
+  let phrase = $derived(httpCode ? phraseForHttpCode(httpCode) : '');
+
+  const missingElements = {
+    imageBlock: 1 << 0,
+    codeBlock: 1 << 1,
+    phraseBlock: 1 << 2
+  };
+
+  const flags = Object.values(missingElements);
+
+  const gameModes = {
+    ...missingElements,
+    anyOne: 1 << 3,
+    anyTwo: (1 << 3) | (1 << 4)
+  };
+
+  let missingFlag = $state(0);
+  let gameMode = gameModes.codeBlock;
+
+  function draw() {
+    httpCode = randomHttpCode();
+    if (gameMode & gameModes.anyOne) {
+      const indexPool = [0, 1, 2];
+      const firstIdx = indexPool.splice(Math.floor(Math.random() * 3))[0];
+      missingFlag = flags[firstIdx];
+      if (gameMode & gameModes.anyTwo) {
+        const secondIdx = indexPool.splice(Math.floor(Math.random() * 2))[0];
+        missingFlag |= flags[secondIdx];
+      }
+    } else {
+      missingFlag = gameMode;
+    }
+  }
+
+  setTimeout(draw, 100);
 </script>
 
-<div class="flex justify-center">
-  <img src={randomImage()} height="500px" alt="" />
-</div>  
+{#snippet imageBlock(/** @type { string } */ src, missing = false)}
+  {#if missing}
+    <img src="cats/frame.png" class="max-h-200" alt="" />
+  {:else}
+    <img {src} class="max-h-200" alt="" />
+  {/if}
+{/snippet}
+
+{#snippet codeBlock(/** @type { number } */ code, missing = false)}
+  {#if missing}
+    <input class="bg-transparent text-center text-5xl" />
+  {:else}
+    <h2 class="text-5xl">{code}</h2>
+  {/if}
+{/snippet}
+
+{#snippet phraseBlock(/** @type { string } */ text, missing = false)}
+  {#if missing}
+    <input class="bg-transparent text-center text-2xl" />
+  {:else}
+    <p class="text-2xl">{text}</p>
+  {/if}
+{/snippet}
+
+{#if httpCode}
+  <div transition:scale class="flex flex-col items-center gap-4">
+    {@render imageBlock(imageSrc, !!(missingFlag & missingElements.imageBlock))}
+    <div class="flex flex-col items-center">
+      {@render codeBlock(httpCode, !!(missingFlag & missingElements.codeBlock))}
+      {@render phraseBlock(phrase, !!(missingFlag & missingElements.phraseBlock))}
+    </div>
+  </div>
+{/if}
--- src/lib/game.js
+++ src/lib/game.js
@@ -1,17 +1,36 @@
 import md5 from 'crypto-js/md5';
 import { StatusCodes } from 'http-status-codes';
 
-const IMAGE_URI_TEMPLATE = "cats/{}.jpg"
+const IMAGE_URI_TEMPLATE = "cats/{}.jpg";
 const EXCLUDED_CODES = ["419", "420"];
+const UPPERCASE_WORDS = ["URI", "HTTP", "OK"];
 const HTTP_CODES = Object.keys(StatusCodes).filter(key => key.length == 3);
 EXCLUDED_CODES.forEach(code => HTTP_CODES.splice(HTTP_CODES.indexOf(code), 1));
 
-export function randomHTTPCode() {
-    const idx = Math.floor(Math.random() * HTTP_CODES.length);
-    return HTTP_CODES[idx];
+export function randomHttpCode() {
+    return Number(HTTP_CODES[Math.floor(Math.random() * HTTP_CODES.length)]);
 }
 
-export function randomImage() {
-    const hash = md5(randomHTTPCode()).toString();
-    return IMAGE_URI_TEMPLATE.replace("{}", hash);
+/**
+ * @param {number} httpCode
+ * @param {boolean} useMd5
+ */
+export function imageForHttpCode(httpCode, useMd5 = true) {
+    const httpCodeString = httpCode.toString();
+    return IMAGE_URI_TEMPLATE.replace("{}", useMd5 ? md5(httpCodeString).toString() : httpCodeString);
+}
+
+/**
+ * @param {string} word
+ */
+function mapPhraseWord(word) {
+    return UPPERCASE_WORDS.includes(word) ? word : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+
+/**
+ * @param {number} httpCode
+ */
+export function phraseForHttpCode(httpCode) {
+    return StatusCodes[httpCode].split("_").map(mapPhraseWord).join(" ");
 }
